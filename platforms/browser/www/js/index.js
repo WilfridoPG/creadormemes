9e1451360237256b39faf87f986000b01fb01d97
wilfridoPG <wile_54@hotmail.es>
2018-06-19 14:03:01 -0500

    interfaz terminado

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -25,7 +25,11 @@ var app = {
 };
 
 app.initialize();
-
+$(".Nuevo").click(function () {
+    cambiarImagen('img/activo/inicio.png','img/inactivo/fondos.png','img/inactivo/personajes.png','img/inactivo/globos.png','img/inactivo/texto.png','img/inactivo/editar.png');
+    cambiarColor("#ffbc00"," #b0b0b1"," #b0b0b1"," #b0b0b1","#b0b0b1","#b0b0b1");
+    mostrarSecciones("#mostrarinicio","#mostrarfondos","#mostrarpersonajes","#mostrarglobos","#mostrartexto","#mostrarfiltros");
+})
 $(".Inicio").click(function () {
     cambiarImagen('img/activo/inicio.png','img/inactivo/fondos.png','img/inactivo/personajes.png','img/inactivo/globos.png','img/inactivo/texto.png','img/inactivo/editar.png');
     cambiarColor("#ffbc00"," #b0b0b1"," #b0b0b1"," #b0b0b1","#b0b0b1","#b0b0b1");
@@ -40,7 +44,6 @@ $(".Personajes").click(function () {
         cambiarImagen('img/inactivo/inicio.png','img/inactivo/fondos.png','img/activo/personajes.png','img/inactivo/globos.png','img/inactivo/texto.png','img/inactivo/editar.png');
         cambiarColor("#b0b0b1"," #b0b0b1"," #ffbc00"," #b0b0b1"," #b0b0b1","#b0b0b1");
         mostrarSecciones("#mostrarpersonajes","#mostrarfondos","#mostrarinicio","#mostrarglobos","#mostrartexto","#mostrarfiltros");
-
 })
 $(".Globos").click(function () {
         cambiarImagen('img/inactivo/inicio.png','img/inactivo/fondos.png','img/inactivo/personajes.png','img/activo/globos.png','img/inactivo/texto.png','img/inactivo/editar.png');
@@ -58,7 +61,6 @@ $(".Editar").click(function () {
         mostrarSecciones("#mostrarfiltros","#mostrartexto","#mostrarglobos","#mostrarpersonajes","#mostrarfondos","#mostrarinicio");
 })
 function cambiarImagen(inicio,fondos,personajes,globos,texto,editar){
-
     $(".Inicio").attr('src', inicio);
     $(".Fondos").attr('src', fondos);
     $(".Personajes").attr('src', personajes);
@@ -66,9 +68,7 @@ function cambiarImagen(inicio,fondos,personajes,globos,texto,editar){
     $(".Texto").attr('src', texto);
     $(".Editar").attr('src', editar);
 }
-
 function cambiarColor(inicio,fondos,personajes,globos,texto,editar){
-
     $(".Inicio").css("color",inicio);
     $(".Fondos").css("color",fondos);
     $(".Personajes").css("color",personajes);
@@ -82,8 +82,63 @@ function mostrarSecciones(activo,inactivo1,inactivo2,inactivo3,inactivo4,inactiv
       $(inactivo2).hide();
       $(inactivo3).hide();
       $(inactivo4).hide();
-      $(inactivo5).hide();
-
-         
-   }
+      $(inactivo5).hide();         
+}
+$(".Filtros").click(function () {
+       $(".Filtros").css("color", "#ffbc00");
+       $(".Filtros").attr('src', 'img/activo/filtros.png');
+       $(".Combinar").css("color", "#b0b0b1");
+       $(".Combinar").attr('src', 'img/inactivo/combinar.png');
+       $("#mostrarfiltrosf").show();
+       $("#mostrarfiltrosc").hide();
+})
+$(".Combinar").click(function () {
+       $(".Filtros").css("color", "#b0b0b1");
+       $(".Filtros").attr('src', 'img/inactivo/filtros.png');
+       $(".Combinar").css("color", "#ffbc00");
+       $(".Combinar").attr('src', 'img/activo/combinar.png');
+        $("#mostrarfiltrosc").show();
+       $("#mostrarfiltrosf").hide();
+       cambiarColorf("#ffbc00","#b0b0b1","#b0b0b1","#b0b0b1");
+       cambiarImagenf('img/activo/brillo.png','img/inactivo/transparencia.png','img/inactivo/pixel.png','img/inactivo/opacidad.png');
+       mostrarango("#brillorango","#transparenciarango","#pixelrango","#opacidadrango");
+})
+$(".Brillo").click(function () { 
+        cambiarColorf("#ffbc00","#b0b0b1","#b0b0b1","#b0b0b1");
+        cambiarImagenf('img/activo/brillo.png','img/inactivo/transparencia.png','img/inactivo/pixel.png','img/inactivo/opacidad.png');
+        mostrarango("#brillorango","#transparenciarango","#pixelrango","#opacidadrango");
+})
+$(".Transparencia").click(function () { 
+        cambiarColorf("#b0b0b1","#ffbc00","#b0b0b1","#b0b0b1");
+        cambiarImagenf('img/inactivo/brillo.png','img/activo/transparencia.png','img/inactivo/pixel.png','img/inactivo/opacidad.png');
+        mostrarango("#transparenciarango","#brillorango","#pixelrango","#opacidadrango");
+})
+$(".Pixel").click(function () { 
+        cambiarColorf("#b0b0b1","#b0b0b1","#ffbc00","#b0b0b1");
+        cambiarImagenf('img/inactivo/brillo.png','img/inactivo/transparencia.png','img/activo/pixel.png','img/inactivo/opacidad.png');
+        mostrarango("#pixelrango","#transparenciarango","#brillorango","#opacidadrango");
+})
+$(".Opacidad").click(function () { 
+        cambiarColorf("#b0b0b1","#b0b0b1","#b0b0b1","#ffbc00");
+        cambiarImagenf('img/inactivo/brillo.png','img/inactivo/transparencia.png','img/inactivo/pixel.png','img/activo/opacidad.png');
+        mostrarango("#opacidadrango","#pixelrango","#transparenciarango","#brillorango");
+})
+function cambiarColorf(brillo,transparencia,pixel,opacidad){
+    $(".Brillo").css("color", brillo);
+    $(".Transparencia").css("color", transparencia);
+    $(".Pixel").css("color", pixel);
+    $(".Opacidad").css("color", opacidad);
+}
+function cambiarImagenf(brillo,transparencia,pixel,opacidad){
+    $(".Brillo").attr('src', brillo);
+    $(".Transparencia").attr('src', transparencia);
+    $(".Pixel").attr('src', pixel);
+    $(".Opacidad").attr('src', opacidad);
+}
+function mostrarango(activo,inactivo1,inactivo2,inactivo3){
+      $(activo).show();
+      $(inactivo1).hide();
+      $(inactivo2).hide();
+      $(inactivo3).hide();        
+}
 
